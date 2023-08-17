0ad951124029a17806f35ea3bf8961b73c069ef8
roy_mral <roymral@gmail.com>
2023-08-17 00:51:14 -0600

    Agregando favicon y arreglo de menú hamburguesa

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,14 +1,19 @@
-import electricity_icon from "../assets/img/electricity_icon.svg";
-import hamburguesa_responsive from "../assets/img/hamburguesa_responsive.svg";
+import electricar_icon from "../assets/img/electricar_icon.svg";
+import hamburguer from "../assets/img/hamburguer.svg";
 
 function NavBar() {
+  function handleMenu() {
+    let $menu = document.querySelector(".menu");
+    $menu.classList.toggle("hidden");
+  }
+
   return (
     <nav className="bg-[#0F4072] text-white mx-auto mt-[32px] flex flex-wrap justify-around items-center rounded sm:flex sm:w-[560px] sm:py-0 md:w-[704px] md:justify-around md:py-[15px] lg:w-[960px] lg:justify-around">
       <a href="./index.html">
         <div className="flex items-center flex-shrink-0">
           <img
             className="fill-current mr-2 sm:ml-3 w-[48px] sm:w-[48px] md:w-[68px] lg:w-[78px]"
-            src={electricity_icon}
+            src={electricar_icon}
             alt=""
           />
           <span className="font-bold text-sm sm:text-md md:text-xl lg:text-4xl">
@@ -18,8 +23,11 @@ function NavBar() {
       </a>
       {/* <!-- Hamburguesa responsive --> */}
       <div className="block sm:hidden">
-        <button className="flex items-center px-3 py-2 border button-hamburger">
-          <img src={hamburguesa_responsive} alt="menu_responsive" />
+        <button
+          onClick={handleMenu}
+          className="flex items-center px-3 py-2 w-[70%]"
+        >
+          <img src={hamburguer} alt="menu_responsive" />
         </button>
       </div>
       {/* <!-- Hamburguesa FIN --> */}
