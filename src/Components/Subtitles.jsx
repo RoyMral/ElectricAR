733709752160bab01b352db5567901d2455e981f
roy_mral <roymral@gmail.com>
2023-08-24 16:44:09 -0600

    Corrección ancho de Componente Subtitles

--- src/Components/Subtitles.jsx
+++ src/Components/Subtitles.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 function Subtitles({ title }) {
   return (
-    <h2 className="bg-[#00B5A1] py-2 text-white font-bold mx-auto text-center sm:w-[640px] md:text-3xl md:w-[768px] lg-w-[1024px]">
+    <h2 className="bg-[#00B5A1] py-2 text-white font-bold mx-auto text-center sm:w-[640px] md:text-3xl md:w-[768px] lg:w-[1024px]">
       {title}
     </h2>
   );
