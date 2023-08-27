91eed8fde7e1a3ab17c3a6b0f0d465bf0270b909
roy_mral <roymral@gmail.com>
2023-08-27 17:41:49 -0600

    Agregando responsive a galería

--- src/Components/GroupImages.jsx
+++ src/Components/GroupImages.jsx
@@ -8,7 +8,7 @@ function GroupImages({ arImagesGroup }) {
     <>
       <div
         style={{ backgroundImage: `url(${arImagesGroup[7]})` }}
-        className="bg-center bg-cover bg-no-repeat pt-12 pb-40 mx-auto sm:w-[640px] md:w-[768px] lg:w-[1024px]"
+        className="bg-center bg-cover bg-no-repeat pt-12 pb-40 mx-auto h-[300px] md:h-full sm:w-[640px] md:w-[768px] lg:w-[1024px]"
       >
         <swiper-container
           effect={"coverflow"}
@@ -23,49 +23,49 @@ function GroupImages({ arImagesGroup }) {
         >
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[0]}
               alt="biomasa"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[1]}
               alt="eolica"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[2]}
               alt="eolica-solar"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[3]}
               alt="geotermica-laspailasII"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[4]}
               alt="hidro-toro3"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[5]}
               alt="represa"
             />
           </swiper-slide>
           <swiper-slide>
             <img
-              className="md:w-[300px] md:h-[300px] opacity-90"
+              className="w-[180px] h-[180px] md:w-[300px] md:h-[300px] opacity-90"
               src={arImagesGroup[6]}
               alt="solarUno"
             />
--- src/Components/GroupImagesStatic.jsx
+++ src/Components/GroupImagesStatic.jsx
@@ -3,43 +3,43 @@ import PropTypes from "prop-types";
 function GroupImagesStatic({ arImagesGroupFour }) {
   return (
     <>
-      <div className="mx-auto my-5 flex flex-wrap justify-center gap-4 sm:w-[640px] md:w-[768px] lg:w-[1024px]">
-        <div className="w-[300px] h-[300px]">
+      <div className="mx-auto my-5 flex flex-col sm:flex-row flex-wrap justify-center items-center gap-4 sm:w-[640px] md:w-[768px] lg:w-[1024px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500  hover:scale-[200%]"
             src={arImagesGroupFour[0]}
             alt=""
           />
         </div>
-        <div className="w-[300px] h-[300px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500 hover:scale-[200%]"
             src={arImagesGroupFour[1]}
             alt=""
           />
         </div>
-        <div className="w-[300px] h-[300px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500 hover:scale-[200%]"
             src={arImagesGroupFour[2]}
             alt=""
           />
         </div>
-        <div className="w-[300px] h-[300px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500 hover:scale-[200%]"
             src={arImagesGroupFour[3]}
             alt=""
           />
         </div>
-        <div className="w-[300px] h-[300px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500 hover:scale-[200%]"
             src={arImagesGroupFour[4]}
             alt=""
           />
         </div>
-        <div className="w-[300px] h-[300px]">
+        <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px]">
           <img
             className="w-full h-full duration-500 hover:scale-[200%]"
             src={arImagesGroupFour[5]}
