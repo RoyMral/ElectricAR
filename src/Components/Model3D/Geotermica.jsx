7763d1ae9414c0502a727327aaee356e54d85b4f
roy_mral <roymral@gmail.com>
2024-03-07 01:38:54 -0600

    Probando RA camara

--- src/Components/Model3D/Geotermica.jsx
+++ src/Components/Model3D/Geotermica.jsx
@@ -1,14 +1,14 @@
 function Geotermica() {
-  <div style={{ margin: "0px", overflow: "hidden" }}>
-    <a-scene arjs>
+  return (
+    <a-scene>
       <a-marker preset="hiro">
         <a-entity position="0 0 0" scale="0.1 0.1 0.1">
           <a-sphere color="blue" radius="5"></a-sphere>
         </a-entity>
       </a-marker>
-      <a-entity camera position="0 0 0"></a-entity>
+      <a-entity camera></a-entity>
     </a-scene>
-  </div>;
+  );
 }
 
 export default Geotermica;
